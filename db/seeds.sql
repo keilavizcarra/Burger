bfa8f6a02a1b9832a3bfa31374faa65da13a0620
Keila Vizcarra <keilavizcarra11@gmail.com>
2019-09-28 14:07:37 -0700

    files update

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burgers_db, devoured) VALUES ("Cheese Burger", false);
+INSERT INTO burgers (burgers_db, devoured) VALUES ("Hamburger Burger", false);
+INSERT INTO burgers (burgers_db, devoured) VALUES ("Vegetarian Burger", false);
